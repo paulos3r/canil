874d750d52ff944ed2a725fa85bb7102cc3cd742
Paulo Sérgio <036paulo@gmail.com>
2022-03-19 00:06:11 -0300

    foi adicionado uma pasta helpers para manipulação de funções do objeto menu ativo, feito o importe na views dinamica de acordo com o que o cliente selecionar.

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,16 +1,42 @@
-import { Request, Response} from 'express'; 
-
+import { Request, Response} from 'express';
+import { createMenuObject } from '../helpers/createMenuObject';
 
 export const home = (req: Request, res:Response) =>{
     //teste res.send('home no controller');
-    res.render('pages/page');
+    res.render('pages/page',{
+        manu: createMenuObject('all'),
+        banner:{
+            title:'Todos os animais',
+            backgound:'allanimals.jpg'
+        }
+    });
 }
 export const dogs = (req: Request, res:Response) =>{
-    //res.render('pages/home');
+    res.render('pages/page', {
+        manu: createMenuObject('dog'),
+        banner:{
+            title:'cachorros',
+            backgound:'banner_dog.jpg'
+        }
+    });
 }
 export const cats = (req: Request, res:Response) =>{
     //res.render('pages/home');
+    res.render('pages/page', {
+        manu: createMenuObject('cat'),
+        banner:{
+            title:'Gatos',
+            backgound:'banner_cat.jpg'
+        }
+    });
 }
 export const fishes = (req: Request, res:Response) =>{
     //res.render('pages/home');
+    res.render('pages/page', {
+        manu: createMenuObject('fish'),
+        banner:{
+            title:'peixes',
+            backgound:'banner_fish.jpg'
+        }
+    });
 }
