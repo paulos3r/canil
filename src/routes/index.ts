fa84b3abe5453949c6a838cb39463793ff7d0c92
Paulo Sérgio <036paulo@gmail.com>
2022-03-18 22:52:19 -0300

    foi adicionado a pasta e os arquivos de controller, adicionando os arquivos iniciais das rotas da pagina contendo as inportações iniciais do projeto para inicio da interface

---
+++ src/routes/index.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import * as PageController from '../controllers/pageController';
+import * as SearchController from '../controllers/sarchController';
+
+
+const router = Router();
+
+router.get('/', PageController.home);
+router.get('/dogs', PageController.dogs);
+router.get('/cats', PageController.cats);
+router.get('/fishes', PageController.fishes);
+
+router.get('/search', SearchController.search)
+
+
+
+export default router;
