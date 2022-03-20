305119684eb6301006068b906cd23ab69bcadd2a
Paulo Sérgio <036paulo@gmail.com>
2022-03-20 15:56:53 -0300

    Foi atualizado a parte do leout da tela de entre em contato, foi alterado para apresentar a região de doação de pet, foi ajustado a base, foi modificado o css para adapitar melhor a tela

--- src/models/pets.ts
+++ src/models/pets.ts
@@ -2,14 +2,15 @@ import {} from './bancoPet';
 
 let constPet;
 type PetType =  'dog'|'cat'|'fish';
-type PetSex = 'Masculino'|'Feminino';
+type PetSex = 'Macho'|'Fêmea'|'';
 
 type Pet = {
     type: PetType,
     image:string,
     name:string,
     color:string,
-    sex: PetSex
+    sex: PetSex,
+    localidade:string
 };
 
 const data: Pet[] = [
@@ -18,119 +19,136 @@ const data: Pet[] = [
         image: 'pastor-alemao.jpg',
         name: 'Pastor-alemão',
         color: 'Amarelo e Preto',
-        sex: 'Masculino'
+        sex: 'Macho',
+        localidade: 'Goiânia - GO'
     },
     {
         type: 'dog',
         image: 'labrador.jpg',
         name: 'Labrador-retriever',
         color: 'Branco',
-        sex: 'Masculino'
+        sex: 'Macho',
+        localidade: 'Goiânia - GO'
     },
     {
         type: 'dog',
         image: 'zwergspitz.jpg',
         name: 'Zwergspitz',
         color: 'Amarelo',
-        sex: 'Feminino'
+        sex: 'Fêmea',
+        localidade: 'Goiânia - GO'
     },
     {
         type: 'dog',
         image: 'husky.jpg',
         name: 'Husky Siberiano',
         color: 'Branco e Preto',
-        sex: 'Masculino'
+        sex: 'Macho',
+        localidade: 'Goiânia - GO'
     },
     {
         type: 'dog',
         image: 'golden.jpg',
         name: 'Golden Retriever',
         color: 'Amarelo',
-        sex: 'Masculino'
+        sex: 'Macho',
+        localidade: 'Goiânia - GO'
     },
     {
         type: 'dog',
         image: 'poodle.jpg',
         name: 'Poodle',
         color: 'Branco',
-        sex: 'Feminino'
+        sex: 'Fêmea',
+        localidade: 'Goiânia - GO'
     },
     {
         type: 'dog',
         image: 'bulldog.jpg',
         name: 'Bulldog',
         color: 'Branco e Amarelo',
-        sex: 'Masculino'
+        sex: 'Macho',
+        localidade: 'Goiânia - GO'
     },
     {
         type: 'cat',
         image: 'persa.jpg',
         name: 'Persa',
         color: 'Amarelo',
-        sex: 'Masculino'
+        sex: 'Macho',
+        localidade: 'Goiânia - GO'
     },
     {
         type: 'cat',
         image: 'mainecoon.jpg',
         name: 'Maine Coon',
         color: 'Preto e Branco',
-        sex: 'Masculino'
+        sex: 'Macho',
+        localidade: 'Goiânia - GO'
     },
     {
         type: 'cat',
         image: 'bengal.jpg',
         name: 'Bengal',
         color: 'Branco, Preto e Amarelo',
-        sex: 'Feminino'
+        sex: 'Fêmea',
+        localidade: 'Goiânia - GO'
     },
     {
         type: 'cat',
         image: 'siames.jpg',
         name: 'Siamês',
         color: 'Amarelo e Preto',
-        sex: 'Masculino'
+        sex: 'Macho',
+        localidade: 'Goiânia - GO'
     },
     {
         type: 'cat',
         image: 'sphynx.jpg',
         name: 'Sphynx',
         color: 'Branco',
-        sex: 'Masculino'
+        sex: 'Macho',
+        localidade: 'Goiânia - GO'
     },
     {
         type: 'fish',
         image: 'neon.jpg',
         name: 'Tetra Neon',
         color: 'Vermelho e Azul',
-        sex: 'Masculino'
+        sex: 'Macho',
+        localidade: 'Goiânia - GO'
     },
     {
         type: 'fish',
         image: 'matogrosso.jpg',
         name: 'Mato Grosso',
         color: 'Laranja',
-        sex: 'Masculino'
+        sex: 'Macho',
+        localidade: 'Goiânia - GO'
     },
     {
         type: 'fish',
         image: 'limpavidro.jpg',
         name: 'Limpa Vidro',
         color: 'Verde e Branco',
-        sex: 'Masculino'
+        sex: 'Macho',
+        localidade: 'Goiânia - GO'
     },
     {
         type: 'fish',
         image: 'tanictis.jpg',
         name: 'Tanictis',
         color: 'Vermelho',
-        sex: 'Masculino'
+        sex: 'Macho',
+        localidade: 'Goiânia - GO'
     },
     {
         type: 'fish',
         image: 'acara.jpg',
         name: 'Acará Bandeira',
         color: 'Preto',
-        sex: 'Masculino'
+        sex: 'Macho',
+        localidade: 'Goiânia - GO'
     },
 ];
 
